7754fcecdd9289aeb3ab68117d8f359a7ec27e27
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-06 22:50:40 -0700

    Updated Person to use props

--- src/App.js
+++ src/App.js
@@ -3,17 +3,31 @@ import './App.css';
 
 import Person from "./Person/Person.js";
 
-function App() {
-   return (
-      <div className="App">
-         <h1>Hi, I'm a React App</h1>
-         <p>This is really working!</p>
-         <Person name="Max" age="28" />
-         <Person name="Manu" age="29">My Hobbies: Racing</Person>
-         <Person name="Stephanie" age="26" />
-      </div>
-   );
-   // return React.createElement("div", {className: "App"}, React.createElement("h1", null, "Hi, I'm a React App :)"));
+class App extends React.Component {
+   render () {
+      return (
+         <div className="App">
+            <h1>Hi, I'm a React App</h1>
+            <p>This is really working!</p>
+            <Person name="Max" age="28" />
+            <Person name="Manu" age="29">My Hobbies: Racing</Person>
+            <Person name="Stephanie" age="26" />
+         </div>
+      );
+   }
 }
+
+// function App() {
+//    return (
+//       <div className="App">
+//          <h1>Hi, I'm a React App</h1>
+//          <p>This is really working!</p>
+//          <Person name="Max" age="28" />
+//          <Person name="Manu" age="29">My Hobbies: Racing</Person>
+//          <Person name="Stephanie" age="26" />
+//       </div>
+//    );
+//    // return React.createElement("div", {className: "App"}, React.createElement("h1", null, "Hi, I'm a React App :)"));
+// }
 
 export default App;
