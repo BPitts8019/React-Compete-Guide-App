1b1e8fe615880dc6f6dd5050ffd88a9ea90ed195
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-13 18:01:05 -0700

    Styled Person cards

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Person.css";
 
 const Person = props => {
    return (
-      <div>
+      <div className="Person">
          <p>I'm {props.name} and I am {props.age} years old!</p>
          {(props.children)? <p>{props.children}</p> : null}
       </div>
