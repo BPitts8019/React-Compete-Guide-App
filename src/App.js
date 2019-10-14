f6677efdb8335b13b7078664082293e7cfc3939d
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-13 17:43:06 -0700

    Restructured for easier reading; prepping for styles

--- src/App.js
+++ src/App.js
@@ -29,9 +29,18 @@ class App extends React.Component {
             <h1>Hi, I'm a React App</h1>
             <p>This is really working!</p>
             <button onClick={this.switchNameHandler}>Switch Name</button>
-            <Person name={this.state.people[0].name} age={this.state.people[0].age} />
-            <Person name={this.state.people[1].name} age={this.state.people[1].age}>My Hobbies: Racing</Person>
-            <Person name={this.state.people[2].name} age={this.state.people[2].age} />
+            <Person 
+               name={this.state.people[0].name} 
+               age={this.state.people[0].age} 
+            />
+            <Person 
+               name={this.state.people[1].name} 
+               age={this.state.people[1].age}
+            >My Hobbies: Racing</Person>
+            <Person 
+               name={this.state.people[2].name} 
+               age={this.state.people[2].age} 
+            />
          </div>
       );
    }
