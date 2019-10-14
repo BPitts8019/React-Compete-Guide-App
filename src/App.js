f38fe5a463989b229ee8de23ca44b0b4886c742c
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-13 18:07:09 -0700

    A different way to style a component

--- src/App.js
+++ src/App.js
@@ -24,11 +24,22 @@ class App extends React.Component {
    };
 
    render () {
+      const buttonStyle = {
+         backgroundColor: "white",
+         font: "inherit",
+         border: "1px solid blue",
+         padding: "8px",
+         cursor: "pointer"
+      };
+
       return (
          <div className="App">
             <h1>Hi, I'm a React App</h1>
             <p>This is really working!</p>
-            <button onClick={this.switchNameHandler}>Switch Name</button>
+            <button 
+               style={buttonStyle}
+               onClick={this.switchNameHandler}
+            >Switch Name</button>
             <Person 
                name={this.state.people[0].name} 
                age={this.state.people[0].age} 
